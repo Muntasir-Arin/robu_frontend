eb573c0ccf0fa9155cf8a9eb6a07f74ac10bcbec
Muntasir-Arin <muntasirarin@gmail.com>
2024-01-01 09:10:55 +0600

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <div>
       <NavBar />
       <div className="pt-10 mt-52">
-        <p className="text-center text-[2.8rem] sm:text-4xl lg:text-5xl font-black uppercase">
+        <p className="text-center text-[2.5rem] sm:text-4xl lg:text-5xl font-black uppercase">
           Transforming Tomorrow
         </p>
-        <p className="uppercase text-center font-black text-transparent text-4xl sm:text-5xl lg:text-7xl bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">
+        <p className="uppercase text-center font-black text-transparent text-[2rem] sm:text-5xl lg:text-7xl bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">
           One Bot at a Time
         </p>
       </div>
