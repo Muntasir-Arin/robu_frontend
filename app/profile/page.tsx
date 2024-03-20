7df33d2f8b20172a144e765dd689f4c89fbf7043
Muntasir-Arin <muntasirarin@gmail.com>
2024-03-20 17:12:04 +0600

    Commit message describing the changes in the submodule

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -16,16 +16,10 @@ import {
     CardTitle,
   } from "@/components/ui/card"
 import Link from 'next/link';
+import useAuth from '@/utils/checkauth';
 
 export default function Profile()  {
-  const [userData, setUserData] = useState({
-    id: '',
-    email: '',
-    name: '',
-    date_of_birth: '',
-    organization: '',
-    avatar: '',
-  });
+  const { handleLogout, isLoggedIn, userData } = useAuth();
 
   
 
@@ -36,18 +30,18 @@ export default function Profile()  {
       <div className=" grid grid-cols-2 gap-2"> 
         <div className="p-6">
             <Avatar className='h-[20rem] w-[20rem]'>
-            <AvatarImage src={userData.avatar}  alt="@shadcn" />
-            <AvatarFallback>{userData.name}</AvatarFallback>
+            <AvatarImage src={userData?.avatar}  alt="@shadcn" />
+            <AvatarFallback>{userData?.name}</AvatarFallback>
             </Avatar>
         </div>
         <div className="mt-20">
-          <h1 className="text-3xl font-bold">  {userData.name}</h1>
-          <p className="text-gray-600 "> Organization: {userData.organization}</p>
+          <h1 className="text-3xl font-bold">  {userData?.name}</h1>
+          <p className="text-gray-600 "> Organization: {userData?.org}</p>
           <p className="text-gray-600">
-          <span className="">Email:</span> {userData.email}
+          <span className="">Email:</span> {userData?.email}
         </p>
         <p className=" text-gray-600">
-          <span className="">Date of Birth:</span> {userData.date_of_birth}
+          <span className="">Date of Birth:</span> {userData?.date_of_birth}
         </p>
         </div>
       </div>
